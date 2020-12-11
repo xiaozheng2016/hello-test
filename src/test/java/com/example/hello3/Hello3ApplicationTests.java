f66b2ab4d080c78af35dfbf9bdc4959d7208d9ac
zhenghongbin <924155240@qq.com>
2020-12-11 18:26:48 +0800

    项目初始化操作

---
+++ src/test/java/com/example/hello3/Hello3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
